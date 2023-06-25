f0769a657befadca2e7e19369bd992ed8e23d326
ibntofajjal <joynal.bintofajjal@gmail.com>
2023-06-25 12:55:43 +0600

    initial Server Setting

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import app from "./app";
+const port = 5000;
+async function main() {
+  try {
+    mongoose.connect("mongodb://127.0.0.1:27017/boilerplate");
+    console.log("••••••••• DATABASE CONNECTED!🎉 ••••••••••");
+
+    app.listen(port, () => {
+      console.log(`🏃‍♂️ App Running On:  ${port}`);
+    });
+  } catch (error) {
+    console.log("⚠️ Connection Error: ", error);
+  }
+}
+
+main();
